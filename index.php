6e7d59e75c99d2e92c67f4671e3082a3d511f684
homker <liu15jia@163.com>
2014-10-12 20:01:44 +0800

    add

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-	echo "hello world!";
+	//echo "hello world!";
+	$a = 1;
+	$array = array('0'=>'1','1'=>'2');
+	//echo $array['0'];
+
+	function big($x,$y)
+	{
+		if($x > $y) {
+			return $X;
+		}else{
+			return $y;
+		}
+	}
+	$big =  big(1,2);
+	echo $big."<br/>";
 ?>
