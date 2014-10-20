b2d5b82a1aac7f0f49581ee73d53159a753948ce
homker <liu15jia@163.com>
2014-10-20 21:26:28 +0800

    fix match

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	exec("nmap -sP 192.168.1.0/24",$res,$rs);
 	if($rs == 0){
 		$param = "/Nmap scan report for(.*?)/\n/";   
-		$result = preg_macth_all($res,$param,$preg);
+		$result = preg_match_all($res,$param,$preg);
 		var_dump($preg);
 		print_r($res);
 	}
