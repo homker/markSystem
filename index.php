6ab8a439c7ff934dc1835ab0d5c7de965999f0df
homker <liu15jia@163.com>
2014-10-20 21:41:37 +0800

    fix

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 	$time = time();
 	exec("nmap -sP 192.168.1.0/24",$res,$rs);
 	if($rs == 0){
-		$param = "/Nmap scan report for(.*?)/\n/";   
-		$result = preg_match_all($param,$res,$matchs);
-		var_dump($matchs);
+		$param = "/Nmap scan report for (.*?)/";   
+		foreach($res as $value){
+			$result = preg_match($param,$res,$matchs);
+			var_dump($matchs);
 		//print_r($res);
+		}
 	}
 	$latetime = time();
 	echo $time = $latetime - $time;
