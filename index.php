6fa8727ffcf58991eab5b0405f318515abe1f7f0
homker <liu15jia@163.com>
2014-10-20 20:39:57 +0800

    add time

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+	$time = time();
 	exec("nmap -sP 192.168.1.0/24",$res,$rs);
 	if($rs == 0){
-		var_dump($res);
+		print_r($res);
 	}
+	$latetime = time();
+	echo $time = $latetime - $time;
 ?>
