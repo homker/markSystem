9d8ce4d4a967c56778a9dfe5c7de66e15f82f16f
homker <liu15jia@163.com>
2014-10-21 22:34:36 +0800

    fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	//var_dump($res);
 	if($rs == 0){
 		foreach($res as $value){
-			if(strstr($value,"192.168.1.106")||strstr($value,"192.168.1.1")) continue;
+			if(strstr($value,"192.168.1.106")) continue;
 			if(strstr($value,"Nmap scan report for")){
 				$result = substr($value,20);
 				array_push($content,$result);
