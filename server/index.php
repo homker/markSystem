d45810525473d9e0492ead9f4cce5861c3d9124e
homker <liu15jia@163.com>
2014-10-23 19:16:17 +0800

    add the redis

--- server/index.php
+++ server/index.php
@@ -1,4 +1,10 @@
 <?php
+
+	//connect the redis
+	
+	$redis= new Redis();
+    $redis->connect('127.0.0.1',6379);
+    
 	$time = microtime();
 	//var_dump($_SERVER['REQUEST_METHOD']);
 	switch($_SERVER['REQUEST_METHOD']){
@@ -20,7 +26,8 @@
 		//echo "hello world";
 	}
 	
-	function  getMAC(){
+	function  getNowMAC(){
+		if(
 		$MAC_address = array();
 		exec("sudo nmap -sP 192.168.1.0/24",$res,$rs);
 		//var_dump($res);
@@ -55,11 +62,52 @@
 		}
 		return $cip;
 	}
+	/*******   redis fucntion   **********/
+	
+	function getMAC($redis){
+		if(!$redis->exists("address")){
+			addMacAddress($redis);
+		}
+		return $tempAddress = $redis->lRange('address', 0, -1);
+	}
+	
+	function addMacAddress($redis)
+	{
+		$macAddress = getNowMAC();
+		//$address = array();
+		for($i = 0; $i<count($macAddress);$i=$i+2){
+			$tmpAddress = serialize(array("IP"=>$macAddress['$i'],"MAC"=>$macAddress['$i+1']));//数组序列化操作
+			$redis->rPush("address",$tmpAddress);
+		}
+		
+	}
+	
+	function getTimelenth($mac,$redis)
+	{
+	
+     $ariveTime = $redis->get('name');
+     $leaveTime = microtime();
+     $timeLenth = $ariveTime - $leaveTime;
+     $redis->sAdd($mac,$timeLenth);
+     return $timeLenth;
+	}
+	
+	function register($mac,$redis)
+	{
+		$registerTime = microtime();
+		if($redis->set($mac,$registerTime)){
+			return true;
+		}else{
+			return false;
+		}
+		
+	}
+	/************* respnse fucntion **************/
 	function response($content,$time)
 	{
 		$latetime = microtime();
 		$time = $latetime - $time;
-		$response = json_encode(array("content"=>$content,"time"=>$time));
+		$response = json_encode(array("content"=>$content,"date"=>date("Y-m-d H:m:s"),"time"=>$time));
 		return $response;
 	}
 	
