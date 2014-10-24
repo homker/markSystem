3cd3483f41fbcaaabc43ed1a69cd388f5ce45c48
homker <liu15jia@163.com>
2014-10-24 20:16:51 +0800

    fix

--- server/index.php
+++ server/index.php
@@ -32,7 +32,7 @@
 			if(is_numeric($_POST['studentID'])){
 			}else{
 				$reback = array("error"=>"not num");
-				echo $content response($reback,$time)
+				echo $content response($reback,$time);
 			}
 		}
 	}
