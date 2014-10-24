2aabf36ce2d4665709e0af2bd7e7832b0ee67b81
homker <liu15jia@163.com>
2014-10-24 17:38:57 +0800

    add index

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	$.get("./server/index.php?MAC=get",function(data){
-		console.log(data.content[0].IP);
 		for(var i = 0 ; i<data.content.length ; i ++ ){
 			var s = i + 1;
 			$(".content").append("<tr><td>"+s+"</td><td>"+data.content[i].IP+"("+data.content[i].MAC+")"+"</td><td>"+data.date+"</td><td>"+data.timelenth+"</td><td><button type='button' class='btn btn-primary btn-sm'>"+data.method+"</button></td></tr>");
